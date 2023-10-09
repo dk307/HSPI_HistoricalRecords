f7f9e8378664dd96bcd0d4273ed18057735baf92
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2023-10-08 18:56:44 -0700

    More tests (#7)
    * More tests

--- tests/Hspi/DevicePageCallbacks.cs
+++ tests/Hspi/DevicePageCallbacks.cs
@@ -7,27 +7,14 @@ using HomeSeer.PluginSdk.Devices.Identification;
 using Hspi;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq.Protected;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HSPI_HistoricalRecordsTest
 {
     [TestClass]
     public class DevicePageCallbacks
     {
-        private class TimeComparer : IComparer<RecordData>
-        {
-            public int Compare(RecordData x, RecordData y) => Comparer<long>.Default.Compare(x.UnixTimeSeconds, y.UnixTimeSeconds);
-        }
-
-        private class ValueComparer : IComparer<RecordData>
-        {
-            public int Compare(RecordData x, RecordData y) => Comparer<double>.Default.Compare(x.DeviceValue, y.DeviceValue);
-        }
-
-        private class StringValueComparer : IComparer<RecordData>
-        {
-            public int Compare(RecordData x, RecordData y) => StringComparer.Ordinal.Compare(x.DeviceString, y.DeviceString);
-        }
-
         public static IEnumerable<object[]> GetDatatableCallbacksData()
         {
             // 1) record limit  = 100, start = 0, length = 10, no ordering specified
@@ -103,9 +90,118 @@ namespace HSPI_HistoricalRecordsTest
              };
         }
 
+        public static IEnumerable<object[]> GetDatatableCallbackTotalData()
+        {
+            // 1) record limit  = 100, start = 0, length = 10, no ordering specified
+            yield return new object[] {
+                new  Func<HsFeature, List<RecordData>, string> ((feature, _) => $"refId={feature.Ref}&recordLimit=100&start=0&length=10"),
+                10, 10
+            };
+
+            // 2) record limit = 100, start = 10, length = 10, no ordering specified
+            yield return new object[] {
+                new Func<HsFeature, List<RecordData>, string> ((feature, _) => $"refId={feature.Ref}&recordLimit=100&start=10&length=50"),
+                10, 10
+            };
+
+            // 3) record limit = 10, start = 10, length = 10, time sort ,asc
+            yield return new object[] {
+                new Func<HsFeature, List<RecordData>, string> ((feature, _) => $"refId={feature.Ref}&recordLimit=100&start=10&length=10&order[0][column]=0&order[0][dir]=asc"),
+               100, 100
+            };
+
+            // 5) min , max, start = 0, length = 10, time sort ,desc
+            yield return new object[] {
+                new Func<HsFeature, List<RecordData>, string> ((feature, records) =>
+                {
+                    records.Sort(new TimeComparer());
+                    var min = records[10].UnixTimeMilliSeconds;
+                    var max = records[30].UnixTimeMilliSeconds;
+                    return $"refId={feature.Ref}&min={min}&max={max}&start=0&length=10&order[0][column]=0&order[0][dir]=desc";
+                }),
+                100, 21
+            };
+
+            // 6) min , max, start = 10, length = 100, value sort ,asc
+            yield return new object[] {
+                new Func<HsFeature, List<RecordData>, string> ((feature, records) =>
+                {
+                    records.Sort(new TimeComparer());
+                    var min = records[20].UnixTimeMilliSeconds;
+                    var max = records[80].UnixTimeMilliSeconds;
+                    return $"refId={feature.Ref}&min={min}&max={max}&start=10&length=100&order[0][column]=1&order[0][dir]=desc";
+                }),
+                100, 61
+             };
+        }
+
+        [TestMethod]
+        public void DatatableCallbackError()
+        {
+            var plugin = TestHelper.CreatePlugInMock();
+            var mockHsController = TestHelper.SetupHsControllerAndSettings(plugin, new Dictionary<string, string>());
+
+            Assert.IsTrue(plugin.Object.InitIO());
+
+            int devRefId = 1938;
+            string paramsForRecord = $"refId={devRefId}&min={1001}&max={99}&start=10&length=100&order[0][column]=1&order[0][dir]=desc";
+
+            string data = plugin.Object.PostBackProc("historyrecords", paramsForRecord, string.Empty, 0);
+            Assert.IsNotNull(data);
+
+            var jsonData = (JObject)JsonConvert.DeserializeObject(data);
+            Assert.IsNotNull(jsonData);
+
+            var errorMessage = jsonData["error"].Value<string>();
+            Assert.IsFalse(string.IsNullOrWhiteSpace(errorMessage));
+
+            plugin.Object.ShutdownIO();
+            plugin.Object.Dispose();
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetDatatableCallbackTotalData), DynamicDataSourceType.Method)]
+        public void DatatableCallbackTotal(Func<HsFeature, List<RecordData>, string> createString, int addedRecordCount, int total)
+        {
+            var plugin = TestHelper.CreatePlugInMock();
+            var mockHsController = TestHelper.SetupHsControllerAndSettings(plugin, new Dictionary<string, string>());
+
+            DateTime nowTime = DateTime.Now;
+
+            var feature = TestHelper.SetupHsFeature(mockHsController, 373, 1.1, displayString: "1.1", lastChange: nowTime,
+                                               apiType: (int)EApiType.Feature);
+
+            Assert.IsTrue(plugin.Object.InitIO());
+
+            var added = new List<RecordData>();
+            for (int i = 0; i < addedRecordCount; i++)
+            {
+                double val = 1000 - i;
+                added.Add(TestHelper.RaiseHSEventAndWait(plugin, Constants.HSEvent.VALUE_CHANGE,
+                                                         feature, val, val.ToString(), nowTime.AddMinutes(i), i + 1));
+            }
+
+            string paramsForRecord = createString(feature, added.Clone());
+
+            string data = plugin.Object.PostBackProc("historyrecords", paramsForRecord, string.Empty, 0);
+            Assert.IsNotNull(data);
+
+            var jsonData = (JObject)JsonConvert.DeserializeObject(data);
+            Assert.IsNotNull(jsonData);
+
+            var recordsTotal = jsonData["recordsTotal"].Value<long>();
+            Assert.AreEqual(total, recordsTotal);
+
+            var recordsFiltered = jsonData["recordsFiltered"].Value<long>();
+            Assert.AreEqual(total, recordsFiltered);
+
+            plugin.Object.ShutdownIO();
+            plugin.Object.Dispose();
+        }
+
         [TestMethod]
         [DynamicData(nameof(GetDatatableCallbacksData), DynamicDataSourceType.Method)]
-        public void DatatableCallbacks(Func<HsFeature, List<RecordData>, string> createString, Func<List<RecordData>, List<RecordData>> filter)
+        public void DatatableCallbackTotalData(Func<HsFeature, List<RecordData>, string> createString, Func<List<RecordData>, List<RecordData>> filter)
         {
             var plugin = TestHelper.CreatePlugInMock();
             var mockHsController = TestHelper.SetupHsControllerAndSettings(plugin, new Dictionary<string, string>());
@@ -165,6 +261,21 @@ namespace HSPI_HistoricalRecordsTest
 
             plugin.Object.ShutdownIO();
             plugin.Object.Dispose();
+        }
+
+        private class StringValueComparer : IComparer<RecordData>
+        {
+            public int Compare(RecordData x, RecordData y) => StringComparer.Ordinal.Compare(x.DeviceString, y.DeviceString);
+        }
+
+        private class TimeComparer : IComparer<RecordData>
+        {
+            public int Compare(RecordData x, RecordData y) => Comparer<long>.Default.Compare(x.UnixTimeSeconds, y.UnixTimeSeconds);
+        }
+
+        private class ValueComparer : IComparer<RecordData>
+        {
+            public int Compare(RecordData x, RecordData y) => Comparer<double>.Default.Compare(x.DeviceValue, y.DeviceValue);
         }
     }
 }
