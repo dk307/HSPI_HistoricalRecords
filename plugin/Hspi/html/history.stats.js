136a072b9a1e3ec04f873762c45f4c1aa9a02663
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2024-01-01 18:07:45 -0800

    Update

--- plugin/Hspi/html/history.stats.js
+++ plugin/Hspi/html/history.stats.js
@@ -1,72 +1,68 @@
-
 function getJsonValue(value, nullSub) {
-	
-	if (value == null) {
-		return nullSub;
-	} else {
-		return roundValue(value.toString()) + " " + deviceUnits;
-	}
+    if (value == null) {
+        return nullSub;
+    } else {
+        return roundValue(value.toString()) + " " + deviceUnits;
+    }
 }
 
 function setUpPersistanceTable() {
-  $('#dt-persistence').dataTable({
-	paging: false,
-	responsive: true,
-	bInfo : false, 
-	searching: false,
-	ajax: function (data, callback, settings) {
-		const formObject = {
-		   refId : featureId,
-		   min: displayStartDate,
-		   max: displayEndDate,   
-		};
-		
-		ajaxPostPlugIn("statisticsforrecords", formObject, function (result) {	
-			const data= result["data"];
-			const jsonResult =
-			{
-				data: [
-					[	
-					 "Average (Step)",
-					 getJsonValue(data[0], '-'),
-					],
-					[	
-					 "Average (Linear)",
-					 getJsonValue(data[1], '-'),
-					],
-					[
-					 "Minimum value",
-					 getJsonValue(data[2], 'No records found'),					 
-					],
-					[	
-					 "Maximum value",
-					 getJsonValue(data[3], 'No records found'),
-					],
-					[	
-					 "Distance between minimum and maximum value",
-					 getJsonValue(data[4], 'No records found'),
-					],
-					[	
-					 "Records count",
-					 data[5],
-					],
-					[	
-					 "Number of time value changed",
-					 data[6],
-					],
-				]
-			};			
-			callback(jsonResult);
-		});
-	},
-  });
-  
-  $(".dataTables_length").addClass("bs-select"); 
+    $('#dt-persistence').dataTable({
+        paging: false,
+        responsive: true,
+        bInfo: false,
+        searching: false,
+        ajax: function (data, callback, settings) {
+            const formObject = {
+                refId: featureId,
+                min: displayStartDate,
+                max: displayEndDate,
+            };
+
+            ajaxPostPlugIn("statisticsforrecords", formObject, function (result) {
+                const data = result["data"];
+                const jsonResult =
+                {
+                    data: [
+                        [
+                            "Average (Step)",
+                            getJsonValue(data[0], '-'),
+                        ],
+                        [
+                            "Average (Linear)",
+                            getJsonValue(data[1], '-'),
+                        ],
+                        [
+                            "Minimum value",
+                            getJsonValue(data[2], 'No records found'),
+                        ],
+                        [
+                            "Maximum value",
+                            getJsonValue(data[3], 'No records found'),
+                        ],
+                        [
+                            "Distance between minimum and maximum value",
+                            getJsonValue(data[4], 'No records found'),
+                        ],
+                        [
+                            "Records count",
+                            data[5],
+                        ],
+                        [
+                            "Number of times value changed",
+                            data[6],
+                        ],
+                    ]
+                };
+                callback(jsonResult);
+            });
+        },
+    });
+
+    $(".dataTables_length").addClass("bs-select");
 }
 
 $(document).ready(function () {
-	setUpPersistanceTable();
-	$('#loading').hide();
+    setUpPersistanceTable();
+    $('#loading').hide();
 });
-
-
